22973db89db2def5455096bc9382ce13b7b409bc
kexplx <oscar.rosner@web.de>
2018-11-09 15:44:52 +0100

    Adds new Splashscreen to show at startup. Splashscreen Contains new theme implemented in styles.xml
    Background Image taken from: https://www.flaticon.com/free-icon/castle_119585

--- app/src/main/java/com/example/oscar/hillfort/HillfortActivity.kt
+++ app/src/main/java/com/example/oscar/hillfort/HillfortActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class HillfortActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
     }
