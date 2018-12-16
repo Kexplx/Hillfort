2117d8c34bdeddc4616b48fc6dc1636f1936b832
kexplx <oscar.rosner@web.de>
2018-12-16 11:32:09 +0100

    adds the new classes Database and PlacmarkDao to prepare for the rooms feature

--- app/src/main/java/com/example/oscar/hillfort/models/SiteModel.kt
+++ app/src/main/java/com/example/oscar/hillfort/models/SiteModel.kt
@@ -1,10 +1,14 @@
 package com.example.oscar.hillfort.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class SiteModel (var id: Long = 0,
+@Entity
+data class SiteModel(
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
                       var title: String = "",
                       var description: String = "",
                       var notes : String = "",
